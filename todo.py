46b2a8616000837c3a626853b328c33b66b75fd5
Srdjan Coric <srdjancoric@MacBook-Pro.local>
2023-11-23 23:52:45 +0100

    create list done

--- todo.py
+++ todo.py
@@ -1,18 +1,32 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, session, render_template, url_for, redirect, request
 app = Flask(__name__)
+
+app.secret_key='secret1'
+
+
+@app.before_request
+def before_request():
+    if 'lists' not in session:
+        session['lists'] = []
 
 @app.route("/")
 def index():
     return redirect(url_for('get_lists'))
 
-@app.route("/lists")
+@app.route("/lists", methods=["GET"])
 def get_lists():
-    lists = [
-        {"name": "Lunch Groceries", "todos": []},
-        {"name": "Dinner Groceries", "todos": []}
+    return render_template('lists.html', lists=session['lists'])
 
-    ]
-    return render_template('lists.html', lists=lists)
+@app.route("/lists", methods=["POST"])
+def create_list():
+    name = request.form["list_name"]
+    session['lists'].append({'name': name, 'todos': []})
+    session.modified = True
+    return redirect(url_for('get_lists'))
+
+@app.route("/lists/new")
+def add_todo():
+    return render_template('new_list.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
