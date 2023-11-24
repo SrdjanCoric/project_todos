31519538f9d3cfc09118da7138054eab676e6aa9
Srdjan Coric <srdjancoric@MacBook-Pro.local>
2023-11-24 11:08:45 +0100

    validations done

--- todo.py
+++ todo.py
@@ -19,11 +19,15 @@ def get_lists():
 
 @app.route("/lists", methods=["POST"])
 def create_list():
-    name = request.form["list_name"]
-    session['lists'].append({'name': name, 'todos': []})
-    flash("The list has been created.", "success")
-    session.modified = True
-    return redirect(url_for('get_lists'))
+    name = request.form["list_name"].strip()
+    if 1 <= len(name) <= 100:
+        session['lists'].append({'name': name, 'todos': []})
+        flash("The list has been created.", "success")
+        session.modified = True
+        return redirect(url_for('get_lists'))
+    else:
+        flash("The list name must be between 1 and 100 characters", "error")
+        return render_template('new_list.html')
 
 @app.route("/lists/new")
 def add_todo():
